699050fa707dc17d7c9a6a60582a99d5a94fb9c1
stask <stask@me.com>
2017-04-18 12:01:43 +0300

    fixed clj-tagsoup dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject insta-looter "0.1.0"
+(defproject insta-looter "0.1.1"
   :description "API-less Instagram scrapper"
   :url "https://github.com/stask/insta-looter/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [clj-http            "2.3.0"]
-                 [clj-tagsoup         "0.3.1"]
+                 [stask/clj-tagsoup   "0.3.1"]
                  [cheshire            "5.7.0"]])
